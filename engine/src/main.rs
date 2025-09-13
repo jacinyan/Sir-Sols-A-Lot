d9cc525cf65e5ba23a9dc4671e6c17d99627234f
jacinyan <jacinjiyan@gmail.com>
2025-09-14 01:16:33 +1000

    test: initial canary tests for rust engine

--- engine/src/main.rs
+++ engine/src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
     println!("Hello, world!");
 }
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn first_test() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn can_connect_to_solana() {
+        assert!(false);
+    }
+}
